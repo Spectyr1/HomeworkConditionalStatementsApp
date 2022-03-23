03fd3b65a4793b281c6f0acc5d4752b5a4cdfbc6
Danny Warren <danny@theforbiddenarts.com>
2022-03-23 00:44:02 -0400

    code submit

--- HomeworkConditionalStatements/Program.cs
+++ HomeworkConditionalStatements/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int age;
+int ageOlder;
+int ageYounger;
+
+
+Console.Write("Hello, please enter your current age: ");
+age = Int32.Parse(Console.ReadLine());
+ageOlder = age + 25;
+ageYounger = age - 25;
+
+Console.WriteLine($"Based on your current age of {age}, you will be {ageOlder} in 25 years. Our math also shows that you were {ageYounger}, 25 years ago.");
+Console.ReadLine();
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 HomeworkConditionalStatementsApp
+
+Capture a user's age from the console and then identify how old they will be in 25 years,
+as well as how old they were 25 years ago. Print that information to the console in natural language.
+
+
+// get age
+// print age + 25 years
+// print age - 25 years
+** use natural language
